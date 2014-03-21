4066d618e84b9922b18610eeafd39f5c75f4901d
shaila Verona Rodriguez <alu0100823257@ull.edu.es>
2014-03-21 10:29:11 +0000

    pract 7

---
+++ src/prct07b.py
@@ -0,0 +1,7 @@
+import prct07a
+
+valores = (10,100,1000,10000,100000,1000000,10000000,100000000,1000000000,10000000000)
+for valor in valores:
+  y=prct07a.aproximacion(valor)
+  print y
+ #el numero maximo de elementos de u.pla es 10000000000
